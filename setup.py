18bf128bd5246ac9f41cb884cfee83695d1dd60c
Joe Schornak <joe.schornak@gmail.com>
2021-06-10 22:39:42 -0400

    initial commit for PickNik proprietary license

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'picknik_ament_copyright'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+    ],
+    install_requires=['setuptools'],
+    package_data={'': [
+        'template/*',
+    ]},
+    zip_safe=False,
+    author='Joe Schornak',
+    author_email='joseph.schornak@picknik.ai',
+    maintainer='Joe Schornak',
+    maintainer_email='joseph.schornak@picknik.ai',
+    url='https://github.com/PickNikRobotics/picknik_ament_copyright',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Check source files for PickNik-specific copyright reference.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'ament_copyright.copyright_name': [
+            'picknik = picknik_ament_copyright.copyright_names:picknik',
+        ],
+        'ament_copyright.license': [
+            'picknik_proprietary = picknik_ament_copyright.licenses:picknik_proprietary',
+        ],
+    },
+)
